ce4ce3a8d64d78e8726af9ce032ab690e846435e
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2025-11-04 02:05:21 +0530

    Adding New Project with Different Approaches

---
+++ youtube_manager_sqlite.py
@@ -0,0 +1,77 @@
+# Here SQlite is a database.
+# Here we create same software that we make on other file but now we use database instend of file handling.
+
+from queue import Empty
+import sqlite3
+
+conn = sqlite3.connect("youtube_videos.db")
+cursor = conn.cursor()
+
+cursor.execute('''                          
+    CREATE TABLE IF NOT EXISTS videos(
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL
+    )
+''')    # Idhar mai Schema banaya hu Database ka kya kya entire lena hai.
+
+def list_all_videos():
+    cursor.execute("SELECT * FROM videos ")
+    rows = cursor.fetchall()
+    if not rows:
+        print("\n File do not have any Videos.....!")
+    else:
+        for row in rows:
+            print(row)
+
+
+def add_videos(name , time):
+    cursor.execute("INSERT INTO videos (name , time) VALUES (? , ?)",(name ,time))     # idhar dhyan dene ke yeah baat hai ki joh (? , ?) usko baad parameter ke hisab se likhna. jaise ki name pahle ? define kar rha hai and time dusra wla ? represent ka rha hai.
+    conn.commit()
+
+
+def update_videos(video_id ,new_name ,new_time):
+    cursor.execute("UPDATE videos SET name = ? ,time = ? WHERE id = ?",(new_name , new_time ,video_id))
+    conn.commit()
+
+
+def delete_videos(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id ,))   # idhar acche se dekhna ek , dekhega usko lagana compulsory hai kyu ki yeah tabhi tuple main jayega aur yeah tuple ke form main hi accept hota hai.
+    conn.commit()
+
+
+
+def main():
+    while True:
+        print("\n Youtube Manager With Database")
+        print("1. List All Videos")
+        print("2. Add Videos")
+        print("3. Update Videos")
+        print("4. Delete Videos")
+        print("5. Exit App")
+        choice = input("\n Enter Your Choice : ")
+
+        match choice:
+
+            case "1":
+                list_all_videos()
+            case "2":
+                name = input("Enter Video Name :")
+                time = input("Enter Video Duration :")
+                add_videos(name , time)
+            case "3":
+                list_all_videos()
+                video_id = input("Enter Video ID to Update Video :")
+                name = input("Enter Video Name :")
+                time = input("Enter Video Duration :")
+                update_videos(video_id , name , time)
+            case "4":
+                video_id = input("Enter Video ID to Delete Video :")
+                delete_videos(video_id)
+            case "5":
+                exit()
+    
+    conn.close()
+
+if __name__ == "__main__":
+    main()
