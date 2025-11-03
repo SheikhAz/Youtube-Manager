17915653e663f37b2c47ada31c9832e1122968ed
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2025-11-04 02:19:51 +0530

    Added Remaining Coded

--- youtube_manager_sqlite.py
+++ youtube_manager_sqlite.py
@@ -27,8 +27,8 @@ def list_all_videos():
 
 def add_videos(name , time):
     cursor.execute("INSERT INTO videos (name , time) VALUES (? , ?)",(name ,time))     # idhar dhyan dene ke yeah baat hai ki joh (? , ?) usko baad parameter ke hisab se likhna. jaise ki name pahle ? define kar rha hai and time dusra wla ? represent ka rha hai.
-    conn.commit()
-
+    conn.commit()    # Yeah karna important hai isko karne se hi database change hoga.
+ 
 
 def update_videos(video_id ,new_name ,new_time):
     cursor.execute("UPDATE videos SET name = ? ,time = ? WHERE id = ?",(new_name , new_time ,video_id))
@@ -37,7 +37,7 @@ def update_videos(video_id ,new_name ,new_time):
 
 def delete_videos(video_id):
     cursor.execute("DELETE FROM videos WHERE id = ?", (video_id ,))   # idhar acche se dekhna ek , dekhega usko lagana compulsory hai kyu ki yeah tabhi tuple main jayega aur yeah tuple ke form main hi accept hota hai.
-    conn.commit()
+    conn.commit()    
 
 
 
@@ -50,24 +50,38 @@ def main():
         print("4. Delete Videos")
         print("5. Exit App")
         choice = input("\n Enter Your Choice : ")
+        print("\n")
 
         match choice:
 
             case "1":
+                print("-"*70)
                 list_all_videos()
+                print("-"*70)
             case "2":
+                print("*"*70)
                 name = input("Enter Video Name :")
                 time = input("Enter Video Duration :")
+                print("*"*70)
                 add_videos(name , time)
+                print("Video Successfully Added into the DATABASE....! \n")
             case "3":
+                print("-"*70)
                 list_all_videos()
+                print("-"*70)
+                print("\n")
+                print("*"*70)
                 video_id = input("Enter Video ID to Update Video :")
                 name = input("Enter Video Name :")
                 time = input("Enter Video Duration :")
+                print("*"*70)
                 update_videos(video_id , name , time)
+                print("Video Successfully Updated on DATABASE......!\n")
             case "4":
+                list_all_videos()
                 video_id = input("Enter Video ID to Delete Video :")
                 delete_videos(video_id)
+                print("Video Successfully Delete From DATABASE....!\n")
             case "5":
                 exit()
     
